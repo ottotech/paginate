f14359f58b16651f72514ea985843416d085a02c
Otto Schuldt <ottoschuldt@gmail.com>
2019-09-21 00:11:55 +0200

    adds filtering functionality to paginator

--- paginate.go
+++ paginate.go
@@ -22,6 +22,7 @@ package paginate
 
 import (
 	"fmt"
+	"log"
 	"net/url"
 	"strconv"
 	"strings"
@@ -30,6 +31,13 @@ import (
 // When initializing a Paginator with NewPaginator this would be the default page size for the pagination.
 const (
 	PageSize = 30
+)
+
+const (
+	gt  = ">"
+	lt  = "<"
+	gte = ">="
+	lte = "<="
 )
 
 // Paginator is the interface that wraps pagination behaviors.
@@ -70,6 +78,7 @@ type pagination struct {
 	pageSize   int
 	tableName  string
 	parameters url.Values
+	filters    []filter
 	colNames   []string
 }
 
@@ -92,48 +101,97 @@ type whereClause struct {
 	exists bool
 }
 
-func NewPaginator(tableName string, colNames []string, params url.Values) Paginator {
+type filterClause struct {
+	clause string
+	args   []interface{}
+	exists bool
+}
+
+type filter struct {
+	field string
+	sign  string
+	value string
+}
+
+func NewPaginator(tableName string, colNames []string, u url.URL) Paginator {
 	var paginator Paginator
+	c := make(chan []filter)
+	decodedURL, err := url.QueryUnescape(u.String())
+	if err != nil {
+		log.Println(err)
+	}
+	go getFilters(decodedURL, colNames, c)
+	v := u.Query()
 	p := new(pagination)
 	p.tableName = tableName
 	p.colNames = colNames
-	p.parameters = params
+	p.parameters = v
 	p.pageSize = PageSize
-	p.request = getRequestData(params)
+	p.filters = <-c
+	p.request = getRequestData(v)
 	paginator = p
 	return paginator
 }
 
-func NewPaginatorWithLimit(pageSize int, tableName string, colNames []string, params url.Values) Paginator {
+func NewPaginatorWithLimit(pageSize int, tableName string, colNames []string, u url.URL) Paginator {
 	var paginator Paginator
+	c := make(chan []filter)
+	decodedURL, err := url.QueryUnescape(u.String())
+	if err != nil {
+		log.Println(err)
+	}
+	go getFilters(decodedURL, colNames, c)
+	v := u.Query()
 	p := new(pagination)
 	p.tableName = tableName
 	p.colNames = colNames
-	p.parameters = params
+	p.parameters = v
 	p.pageSize = pageSize
-	p.request = getRequestData(params)
+	p.filters = <-c
+	p.request = getRequestData(v)
 	paginator = p
 	return paginator
 }
 
 func (p *pagination) Paginate() (sql string, values []interface{}, err error) {
 	var s string
+	var AND = " AND "
+	var WHERE = " WHERE"
 	c1 := make(chan whereClause)
 	c2 := make(chan string)
 	c3 := make(chan string)
+	c4 := make(chan filterClause)
 	go createWhereClause(p.colNames, p.parameters, c1)
 	go createPaginationClause(p.request.pageNumber, p.pageSize, c2)
 	go createOrderByClause(p.parameters, p.colNames, c3)
+	go createFilterClause(p.filters, c4)
 	where := <-c1
 	pagination := <-c2
 	order := <-c3
+	filter := <-c4
 
-	if where.exists {
+	numArgs := len(where.args) + len(filter.args)
+	placeholders := make([]interface{}, 0)
+	for i := 1; i < numArgs+1; i++ {
+		placeholders = append(placeholders, i)
+	}
+	args := make([]interface{}, 0)
+	args = append(args, where.args...)
+	args = append(args, filter.args...)
+
+	if where.exists && filter.exists {
+		s = "SELECT " + strings.Join(p.colNames, ", ") + ", count(*) over() FROM " + p.tableName + where.clause + AND + filter.clause + order + pagination
+		s = fmt.Sprintf(s, placeholders...)
+	} else if where.exists {
 		s = "SELECT " + strings.Join(p.colNames, ", ") + ", count(*) over() FROM " + p.tableName + where.clause + order + pagination
+		s = fmt.Sprintf(s, placeholders...)
+	} else if filter.exists {
+		s = "SELECT " + strings.Join(p.colNames, ", ") + ", count(*) over() FROM " + p.tableName + WHERE + filter.clause + order + pagination
+		s = fmt.Sprintf(s, placeholders...)
 	} else {
 		s = "SELECT " + strings.Join(p.colNames, ", ") + ", count(*) over() FROM " + p.tableName + order + pagination
 	}
-	return s, where.args, nil
+	return s, args, nil
 }
 
 func (p *pagination) Response() PaginationResponse {
@@ -187,14 +245,12 @@ func createWhereClause(colNames []string, v url.Values, c chan whereClause) {
 	var separator string
 	var clauses []string
 	var values []interface{}
-	var placeHolder = 1
 
 	// map all db column names with the url parameters
 	for _, name := range colNames {
 		if val := v.Get(name); val != "" {
 			values = append(values, val)
-			clauses = append(clauses, fmt.Sprintf("%s = $%v", name, placeHolder))
-			placeHolder++
+			clauses = append(clauses, name+" = $%v")
 		}
 	}
 	// use appropriate `separator` to join the clauses
@@ -208,6 +264,34 @@ func createWhereClause(colNames []string, v url.Values, c chan whereClause) {
 	w.args = values
 	w.exists = len(clauses) > 0
 	c <- w
+}
+
+func createFilterClause(filters []filter, c chan filterClause) {
+	var AND = " AND "
+	var s = ""
+	f := filterClause{}
+	args := make([]interface{}, 0)
+	for i, f := range filters {
+		if v, err := strconv.Atoi(f.value); err != nil {
+			args = append(args, v)
+		} else {
+			args = append(args, f.value)
+		}
+
+		if i == 0 {
+			s += fmt.Sprintf(" %s %s ", f.field, f.sign) + "$%v"
+			continue
+		}
+		if i == len(filters)-1 {
+			s += fmt.Sprintf(" %s %s ", f.field, f.sign) + "$%v"
+			continue
+		}
+		s += fmt.Sprintf(" %s %s ", f.field, f.sign) + "$%v" + AND
+	}
+	f.clause = s
+	f.args = args
+	f.exists = s != ""
+	c <- f
 }
 
 func createPaginationClause(pageNumber int, pageSize int, c chan string) {
@@ -262,4 +346,57 @@ func createOrderByClause(v url.Values, colNames []string, c chan string) {
 	clauses = append(clauses, "id")
 	clauseSTR := strings.Join(clauses, ",")
 	c <- " ORDER BY " + clauseSTR
+}
+
+func getFilters(decodedPath string, colNames []string, c chan []filter) {
+	s := make([]filter, 0)
+	i := strings.Index(decodedPath, "?")
+	if i == -1 {
+		c <- s
+		return
+	}
+
+	getF := func(key, val, char string) (bool, filter) {
+		f := filter{}
+		if strings.Contains(val, char) {
+			if val[:len(char)] == char && len(val) > len(char) {
+				f.field = key
+				f.sign = char
+				f.value = val[len(char):]
+				return true, f
+			}
+		}
+		return false, f
+	}
+
+	params := strings.Split(decodedPath[i+1:], "&")
+	for _, n := range colNames {
+		for _, p := range params {
+
+			if len(p) <= len(n) {
+				continue
+			}
+			key, value := p[:len(n)], p[len(n):]
+			if key != n {
+				continue
+			}
+			if ok, f := getF(key, value, gte); ok {
+				s = append(s, f)
+				continue
+			}
+			if ok, f := getF(key, value, lte); ok {
+				s = append(s, f)
+				continue
+			}
+			if ok, f := getF(key, value, gt); ok {
+				s = append(s, f)
+				continue
+			}
+			if ok, f := getF(key, value, lt); ok {
+				s = append(s, f)
+				continue
+			}
+		}
+	}
+	c <- s
 }
