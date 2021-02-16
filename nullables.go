c5385b7c3d0c642fb06b736e608d523211644f4b
Otto Schuldt <ottoschuldt@gmail.com>
2021-02-16 10:18:28 +0100

    updates NullInt.Scan method

--- nullables.go
+++ nullables.go
@@ -30,6 +30,26 @@ func (ni *NullInt) Scan(value interface{}) error {
 		intVal = int(t)
 	case int64:
 		intVal = int(t)
+	case uint:
+		intVal = int(t)
+	case uint8:
+		intVal = int(t)
+	case uint16:
+		intVal = int(t)
+	case uint32:
+		intVal = int(t)
+	case uint64:
+		intVal = int(t)
+	// As an special case when passing custom types for int values that implement the Scanner interface to the
+	// driver "go-sql-driver/mysql", the driver will return []uint8. Therefore, we need to try to parse that returned
+	// value to string and finally to int if possible.
+	// Check issue: https://github.com/go-sql-driver/mysql/issues/441
+	case []uint8:
+		_int, err := strconv.Atoi(string(t))
+		if err != nil {
+			return err
+		}
+		intVal = _int
 	default:
 		return fmt.Errorf("column is not int")
 	}
